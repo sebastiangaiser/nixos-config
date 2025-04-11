f4133571fdc1e639caff7dd074c1aeb1f42c5957
Sebastian Gaiser <sebastian.gaiser@hetzner-cloud.de>
2025-04-11 16:59:05 +0200

    chore: updates

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -1,7 +1,8 @@
-{ config, pkgs, ... }:
+{ config, pkgs, nixpkgs-unstable, ... }:
 {
   programs.vscode = {
     enable = true;
+    #package = pkgs-unstable.vscode;
     #profiles.default = {
     #  userSettings = {
     #    "editor.tabSize" = 2;
