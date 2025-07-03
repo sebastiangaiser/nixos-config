af42c1bd5294c0c8e0260266dd19fabbd1645cdd
Sebastian Gaiser <sebastiangaiser@users.noreply.github.com>
2025-07-03 07:53:56 +0200

    fix(ghostty): workaround slowdowns due to kernel 6.15.4

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -7,5 +7,13 @@
       font-family = "JetBrains Mono";
       font-size = 10;
     };
+    # https://github.com/ghostty-org/ghostty/discussions/7720
+    package = pkgs.ghostty.overrideAttrs (_: {
+       preBuild = ''
+         shopt -s globstar
+         sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
+         shopt -u globstar
+       '';
+     });
   };
 }
