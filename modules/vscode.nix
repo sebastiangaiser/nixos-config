490a4c4f6ef616ac88fff40381bbd98deaae77c0
Sebastian Gaiser <sebastiangaiser@users.noreply.github.com>
2025-07-19 22:56:36 +0200

    feat(vscode): add default config

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -1,18 +1,16 @@
 {
   config,
   pkgs,
-  nixpkgs-unstable,
   ...
 }:
 {
   programs.vscode = {
     enable = true;
-    #package = pkgs-unstable.vscode;
-    #profiles.default = {
-    #  userSettings = {
-    #    "editor.tabSize" = 2;
-    #    "files.autoSave" = "onFocusChange";
-    #  };
-    #};
+    profiles.default = {
+      userSettings = {
+        "editor.tabSize" = 2;
+        "files.autoSave" = "onFocusChange";
+      };
+    };
   };
 }
