9e009e6b7091373fb80b70f94e1cf0df40c0128d
Sebastian Gaiser <sebastiangaiser@users.noreply.github.com>
2026-01-03 16:56:25 +0100

    chore(atuin): remove settings for 'enter_accpet' as now being default

--- modules/atuin.nix
+++ modules/atuin.nix
@@ -3,9 +3,5 @@
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
-    settings = {
-      # TODO not working due to `Existing file '/home/sebastian/.config/atuin/config.toml' would be clobbered`
-      # enter_accept = false;
-    };
   };
 }
