d6b6c72bcac069831c17648f794bb5c8d70976bc
Sebastian Gaiser <sebastiangaiser@users.noreply.github.com>
2026-01-03 18:06:20 +0100

    chore(ghostty): remove fixed workaround

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -7,13 +7,5 @@
       font-family = "JetBrains Mono";
       font-size = 10;
     };
-    # https://github.com/ghostty-org/ghostty/discussions/7720
-    package = pkgs.ghostty.overrideAttrs (_: {
-      preBuild = ''
-        shopt -s globstar
-        sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
-        shopt -u globstar
-      '';
-    });
   };
 }
